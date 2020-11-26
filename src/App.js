21bb049072bfc5f5f41e473ea0f0bdfa8c8216aa
Guillaume Piedigrossi <guillaume.piedigrossi@gadz.org>
2020-11-26 20:12:14 +0100

    :truck: (App) mouve app in a file

--- src/App.js
+++ src/App.js
@@ -1,27 +1,64 @@
-import React from "react"
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
+import FlightTable from "./components/FlightTable";
+import FlightForm from "./components/FlightForm";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      flights: [
+        {
+          date: "2020-01-01",
+          departureTime: "10:00",
+          arrivalTime: "11:00",
+          departureAirport: "LFLN",
+          arrivalAirport: "LFLN",
+          picName: "SELF",
+          functionOnBoard: "pic",
+          aircraftType: "TB20",
+          aircraftRego: "F-GGNV",
+          flightTime: 1,
+          dayLanding: 3,
+          nightLanding: 0,
+          ifrApproach: 0,
+          goAround: 0,
+          opsCondition: "day",
+        },
+      ],
+    };
+  }
+
+  handleChange = (e) => {
+    e.preventDefault();
+
+    const name = e.currentTarget.name;
+    const value = e.currentTarget.value;
+
+    this.setState({ [name]: value });
+  };
+
+  handleAdd = (flight) => {
+    const flights = this.state.flights.slice();
+    flights.push(flight);
+
+    this.setState({ flights: flights });
+  };
+
+  render() {
+    return (
+      <div>
+        <h1> Entrer un nouveau vol </h1>
+
+        <FlightForm onFlightAdd={this.handleAdd} />
+
+        <h2> Résumé des vols entrés </h2>
+
+        <FlightTable details={this.state.flights} />
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,67 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import App from "./App";
 import "./index.css";
 import reportWebVitals from "./reportWebVitals";
-
-import FlightTable from "./components/FlightTable";
-import FlightForm from "./components/FlightForm";
-
-class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      flights: [
-        {
-          date: "2020-01-01",
-          departureTime: "10:00",
-          arrivalTime: "11:00",
-          departureAirport: "LFLN",
-          arrivalAirport: "LFLN",
-          picName: "SELF",
-          functionOnBoard: "pic",
-          aircraftType: "TB20",
-          aircraftRego: "F-GGNV",
-          flightTime: 1,
-          dayLanding: 3,
-          nightLanding: 0,
-          ifrApproach: 0,
-          goAround: 0,
-          opsCondition: "day",
-        },
-      ],
-    };
-  }
-
-  handleChange = (e) => {
-    e.preventDefault();
-
-    const name = e.currentTarget.name;
-    const value = e.currentTarget.value;
-
-    this.setState({ [name]: value });
-  };
-
-  handleAdd = (flight) => {
-    const flights = this.state.flights.slice();
-    flights.push(flight);
-
-    this.setState({ flights: flights });
-  };
-
-  render() {
-    return (
-      <div>
-        <h1> Entrer un nouveau vol </h1>
-
-        <FlightForm onFlightAdd={this.handleAdd} />
-
-        <h2> Résumé des vols entrés </h2>
-
-        <FlightTable details={this.state.flights} />
-      </div>
-    );
-  }
-}
 
 ReactDOM.render(
   <React.StrictMode>
